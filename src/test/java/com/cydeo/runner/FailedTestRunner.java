1c314844fd4d0a39f909ddfcdaeb88e3f5e38f1a
Andrew Gerashchenko <andgerashchenko@gmail.com>
2022-07-11 22:13:20 -0400

    Add runner

---
+++ src/test/java/com/cydeo/runner/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.cydeo.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features= "@target/rerun.txt",
+        glue = "com/cydeo/step_definitions"
+)
+
+public class FailedTestRunner {
+}
