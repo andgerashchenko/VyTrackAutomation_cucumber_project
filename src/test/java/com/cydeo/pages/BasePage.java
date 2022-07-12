40c6cf77b8e73405a811401334f7a7e43246e000
Andrew Gerashchenko <andgerashchenko@gmail.com>
2022-07-11 23:13:36 -0400

    edit pages

--- src/test/java/com/cydeo/pages/BasePage.java
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -1,4 +1,39 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Dashboards')]")
+    public WebElement DashboardModule;
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Fleet')]")
+    public WebElement FleetModule;
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Customers')]")
+    public WebElement CustomersModule;
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Sales')]")
+    public WebElement SalesModule;
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Activities')]")
+    public WebElement ActivitiesModule;
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Marketing')]")
+    public WebElement MarketingModule;
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Reports &')]")
+    public WebElement ReportsSegmentsModule;
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'System')]")
+    public WebElement SystemModule;
+
+    @FindBy(xpath = "//a/span[.='Contacts']")
+    public WebElement Contracts;
+
 }
